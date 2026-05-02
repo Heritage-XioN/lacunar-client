f5ca89b6c1d2ae219f0392706f1ce0bdf845405c
HERITAGE-XION <iheritage934@gmail.com>
2026-05-02 11:57:05 +0100

    hot-fix: fixed consultant table displaying non consultatnt accounts

--- components/block/consultants-table.tsx
+++ components/block/consultants-table.tsx
@@ -6,7 +6,10 @@ import { consultants } from '@/types/consultants';
 import useSWR from 'swr';
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 const ConsultantsTable = () => {
-	const { data } = useSWR<{ data: consultants[] }>('/api/clientlist', fetcher);
+	const { data } = useSWR<{ data: consultants[] }>(
+		'/api/consultantList',
+		fetcher,
+	);
 	return (
 		<div className='container mx-auto bg-[#ffffff]'>
 			<DataTable columns={columns} data={data?.data || []} filterBy={'email'} />
